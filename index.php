5417422195c276c29cde5a51e7be28fdd6dbb0bd
Wagner Lamoglia <wagnyero@gmail.com>
2020-05-19 19:10:01 -0300

    Inicio Projeto

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="" method="post">
+        <input type="text" name="txtLogin" value="admin" placeholder="Login:" required />
+        <input type="password" name="txtPassword" value="12345" placeholder="Password" required />
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
